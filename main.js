a20d39817fee5361aef48ecc879bea811b10c860
durgachanakya <81852237+durgachanakya@users.noreply.github.com>
2022-03-28 19:14:38 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,10 +5,29 @@ function preload(){
 function setup(){
     canvas = createCanvas(300,300);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300) ;
+    video.hide();
+
+    posenet = ml5.poseNet(video,modelLoaded);
+    posenet.on('pose',gotposes);
+}
+
+function modelLoaded(){
+    console.log("posenet is loaded");
+}
+
+function gotposes(results){
+    if(results.length>0){
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nosex = " + noseX + "nosey =" + noseY);
+    }
 }
 
 function draw(){
-
+    image(video,0,0,300,300);
 }
 
 function take_snapshot(){
